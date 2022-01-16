d8b366ca94bc9e43d3eab82ce52e005a51c95367
Cameron Aziz <cameronaziz@me.com>
2022-01-16 07:10:04 -0800

    root

--- src/utils/paths.ts
+++ src/utils/paths.ts
@@ -1,7 +1,4 @@
 import path from 'path';
 
-export const rootDir = (): string =>
-  path.join(process.cwd(), '../..');
-
 export const absolutePath = (relativePath: string): string =>
-  path.join(rootDir(), relativePath);
+  path.join(process.cwd(), relativePath);
